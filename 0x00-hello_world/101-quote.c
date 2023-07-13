dcad0fddf1d17911fa21b61e25eb246e7ad0c3cc
Gamalnabil <gamalnabil556@gmail.com>
2023-07-13 16:48:34 -0700

    he

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
 /**
  * main - Entry point
  *
- * description: print "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+ * description:print"and that piece of art is useful\" - Dora Korpar, 2015-10-19"
  *
  * Return = Always 1 (Success)
  */
