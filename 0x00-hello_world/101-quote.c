f327eaa9ab6251bcab7874cde838ee8b07bfe521
Gamalnabil <gamalnabil556@gmail.com>
2023-07-13 16:18:13 -0700

    hello world!!!!!!!

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return = Always 0 (Success)
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - dora korpar %d %c %d %c %d", 2015-10-19);
+	return (0);
+}
