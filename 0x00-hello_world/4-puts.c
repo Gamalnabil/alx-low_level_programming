d73ad5a22bfa027fe4b371d9d5c01861988ccbd0
Gamalnabil <gamalnabil556@gmail.com>
2023-07-13 14:47:21 -0700

    yarab

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *  main - Print "Programming is like building a multilingual puzzle 
+ *  main - Print Programming is like building a multilingual puzzle 
  *  
  * Return: Always 0 (Success)  
  */ 
