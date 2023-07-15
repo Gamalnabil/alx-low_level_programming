d5d79bfdf449532b8f15dab688a1bfe699b4eba3
Gamalnabil <gamalnabil556@gmail.com>
2023-07-14 18:29:09 -0700

    dvsvs

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int m;
+
+	for (m = 0; m <10; m++)
+	{
+		putcher(m);
+	}
+	putcher('\n');
+	return (0);
+}`
