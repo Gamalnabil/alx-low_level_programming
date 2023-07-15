84f925b2852c8ab0d0fa8302a1db534e1f3fa7e5
Gamalnabil <gamalnabil556@gmail.com>
2023-07-14 19:18:22 -0700

    dsvd v

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n > 10; n++)
+	{
+		putchar(n + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n);
+	return (0);
+}
