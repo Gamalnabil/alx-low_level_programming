9a195f24cd37624b0b20d014b3a25c656e7da153
Gamalnabil <gamalnabil556@gmail.com>
2023-07-18 11:55:14 -0700

    Done

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,30 +8,33 @@ void print_times_table(int n)
 {
 	int prod, mult, num;
 
-	if (num = 0; num <= n; ++num)
+	if (n <= 15 && n >= 0)
 	{
-		_putchar(48);
-		for (mult = 1; mult <= n; ++mult)
+		for (num = 0; num <= n; ++num)
 		{
-			_putchar(',');
-			_putchar(' ');
-
-			prod = num * mult;
-
-			if (prod <= 9)
+			_putchar(48);
+			for (mult = 1; mult <= n; ++mult)
 			{
+				_putchar(',');
 				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 9)
+				{
+					_putchar(' ');
+				}
+				else if (prod >= 100)
+				{
+					_putchar((prod / 100) + 48);
+					_putchar((prod / 10) % 10 + 48);
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + 48);
+				}
+				_putchar('\n');
 			}
-			else if (prod >= 100)
-			{
-				_putchar((prod / 100) + 48);
-				_putchar((prod / 10) % 10 + 48);
-			}
-			else if (prod <= 99 && prod >= 10)
-			{
-				_putchar((prod / 10) + 48);
-			}
-			_putchar('\n');
 		}
 	}
 }
