e4e2c2181fa9168be44762b3b50a15522d07f667
Gamalnabil <gamalnabil556@gmail.com>
2023-07-18 11:50:44 -0700

    Done

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_times_table - this is big time table
+ * @n: this is th numbers of rows
+*/
+void print_times_table(int n)
+{
+	int prod. mult, num;
+
+	if (num = 0; num <= n; ++num)
+	{
+		_putchar(48);
+		for (mult = 1; mult <= n; ++mult)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= 9)
+			{
+				_putchar(' ');
+			}
+			else if (prod >= 100)
+			{
+				_putchar((prod / 100) + 48);
+				_putchar((prod / 10) % 10 + 48);
+			}
+			else if (prod <= 99 && prod >= 10)
+			{
+				_putchar((prod / 10) + 48);
+			}
+			_putchar('\n');
+		}
+	}
+}
