33362c192b7f97ebfe779b399f394d97e1a2331f
Gamalnabil <gamalnabil556@gmail.com>
2023-07-18 06:55:45 -0700

    Done

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int _islower(int c)
+{
+	if (true)
+	{
+		lowercase(c);
+		return (0);
+	}
+	else 
+	{
+		return (1);
+	}
+}	
