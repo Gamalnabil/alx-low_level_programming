947fbf1944d175e3235d5b224ba77d89302a2f45
Gamalnabil <gamalnabil556@gmail.com>
2023-07-18 08:16:03 -0700

    Done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * _abs - get the absolute value of the integer
+ * 
+ * Return: always 0 (Success)
+*/
+int _abs(int)
+{
+	int n;
+
+	if (n < 0)
+	{
+		fabs(n);
+	}
+}
