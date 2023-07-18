0d00efe518809ceb4bbdbe3d8484ac90e585e0ab
Gamalnabil <gamalnabil556@gmail.com>
2023-07-18 01:47:13 -0700

    Done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int p[] = {_, p, u, t, c, h, a, r}
+
+	while (p < 8)
+	{
+		putchar(p + '0');
+		p++
+	}
+	return (0);
+}
