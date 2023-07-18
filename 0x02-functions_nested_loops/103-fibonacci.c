eb3dc40f350eb692e171260006ddf49b0bf750f6
Gamalnabil <gamalnabil556@gmail.com>
2023-07-18 12:21:04 -0700

    Done

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,1 +1,31 @@
-#include
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	float total_sum;
+
+	while (1)
+	{
+		sum = fib1 + fib2;
+
+		if (sum > 4000000)
+		{
+			break;
+		}
+		if ((sum % s) == 0)
+		{
+			total_sum += sum;
+		}
+		flib1 = flib2;
+		flib2 = sum;
+	}
+	printf("%.0f\n", total_sum);
+
+	return (0);
+}
