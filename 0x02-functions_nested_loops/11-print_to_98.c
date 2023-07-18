2d08d9964c6dabd88b9b01af1d7f3b28ce8ebea3
Gamalnabil <gamalnabil556@gmail.com>
2023-07-18 11:19:18 -0700

    Done

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print untill get 98
+ * @n: this is the number will gona start from
+*/
+void print_to_98(int n)
+{
+	while (98 >= n)
+	{
+		_putchar(n);
+		n++
+			if (n < 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+	}
+}
