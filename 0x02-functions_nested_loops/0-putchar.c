e481bdb9482b71e95f5fedd87f2c055c1a6b60c9
Gamalnabil <gamalnabil556@gmail.com>
2023-07-18 05:00:45 -0700

    Done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char p[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r', '\n'};
+
+	_putchar(p, 9);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int _putchar(char p[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		putchar(p[i]);
+	}
+}
