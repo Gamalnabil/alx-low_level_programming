e6fdbda43c00afb5623d8d07ae965d10af4e0825
Gamalnabil <gamalnabil556@gmail.com>
2023-07-20 12:35:11 -0700

    Done

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * main - test negative or positive function
- *
- * Return: Always 0 (Success)
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
+
 int main(void)
 {
 	int i;
 
-	i = 0;
+	i = 98 % 2;
 	positive_or_negative(i);
+
 	return (0);
 }
