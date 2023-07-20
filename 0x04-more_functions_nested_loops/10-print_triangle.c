e13e299efe93a35f3acc9ef89ca02b5b211988d3
Gamalnabil <gamalnabil556@gmail.com>
2023-07-20 09:12:03 -0700

    Done

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: size of triangle
+ * Return: 0
+*/
+void print_triangle(int size)
+{
+	int row, col;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (row = 1; row <= size; row++)
+	{
+		for (col = 1; col <= size; col++)
+		{
+			if ((row + col) <= size)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
