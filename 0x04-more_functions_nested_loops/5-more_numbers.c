08f7eeb3333d7f0571a98d8a4472b3444becda04
Gamalnabil <gamalnabil556@gmail.com>
2023-07-20 07:02:17 -0700

    Done

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,9 +7,11 @@
 */
 void more_numbers(void)
 {
-	for (int n = 0; n < 10; n++)
+	int n, z;
+
+	for (n = 0; n < 10; n++)
 	{
-		for (int z = 0; z <= 14; z++)
+		for (z = 0; z <= 14; z++)
 		{
 			_putchar(z + '0');
 		}
