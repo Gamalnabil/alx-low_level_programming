b00cb27d0eb792b1de311333834eb9472fe1b395
Gamalnabil <gamalnabil556@gmail.com>
2023-07-20 10:43:41 -0700

    Done

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_number - print some numbers
+ * @n: value of num
+*/
+void print_number(int n)
+{
+	unsigned int m  = n;
+
+	if (n < 0)
+	{
+		_putchar(' ');
+		m = -m;
+	}
+	if ((n / 10) > 0)
+	{
+		print_number(m / 10);
+	}
+	_putchar((m % 10) + 48);
+}
