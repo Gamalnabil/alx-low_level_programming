5ef1c7549b54252ba6930c2dca405e54b8524c22
Gamalnabil <gamalnabil556@gmail.com>
2023-07-20 08:06:59 -0700

    Done

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_squar - print squar from #
+ * @size: number of #
+ * Return: Always newline
+*/
+void print_squar(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	for (i = 0; i < size; i++)
+	{
+		for(j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+	}
+	_putchar('\n');
+}
