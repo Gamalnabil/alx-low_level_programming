a3e131cf0f753c10eccba4dbf1bf7ffa7b670f96
Gamalnabil <gamalnabil556@gmail.com>
2023-07-20 06:19:20 -0700

    Done

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - Print numbers from 0 to 14 ten times
+ *
+ * Return: Always 0
+*/
+void more_numbers(void)
+{
+	for (int n = 0; n < 10; n++)
+	{
+		for (int z = 0; z <= 14; Z++)
+		{
+			_putchar(z + '0');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
