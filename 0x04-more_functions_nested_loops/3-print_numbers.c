0f08719e80571bc9e261368f0fb8a9d4f7dd52dd
Gamalnabil <gamalnabil556@gmail.com>
2023-07-20 05:09:12 -0700

    Done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - this prints numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+*/
+void print_numbers(void)
+{
+	for (int n = 0; n < 10; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
