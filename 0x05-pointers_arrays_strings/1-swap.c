3c1957f38adf3f29ee7c6552de04a72a01cad969
Gamalnabil <gamalnabil556@gmail.com>
2023-07-24 09:04:26 -0700

    Done

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * swap_int - this function swap two integers value
+ * @a: this is the first integer value
+ * @b: this is the secound integer value
+*/
+void swap_int(int *a, int *b)
+{
+	*a = *b
