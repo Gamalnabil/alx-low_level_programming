5d8de92d9b794ed6e68ef62bf7de9504ebb2eafa
Gamalnabil <gamalnabil556@gmail.com>
2023-07-24 10:46:17 -0700

    Done

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -21,9 +21,9 @@ int main(void)
 		pass[i] = rand() % 78;
 		s += (pass[i] + '0');
 		putchar(pass[i] + '0');
-		if ((2772 - sum) - '0' < 78)
+		if ((2772 - s) - '0' < 78)
 		{
-			n = 2772 - sum - '0';
+			n = 2772 - s - '0';
 			s += n;
 			putchar(n + '0');
 			break;
