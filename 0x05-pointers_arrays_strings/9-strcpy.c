549072d5a1454254081de0024e9f8a42a5a856e4
Gamalnabil <gamalnabil556@gmail.com>
2023-07-24 10:48:56 -0700

    Done

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,13 +9,13 @@
 char *_strcpy(char *dest, char *src)
 {
 	int l = 0;
-	int x = 0;
+	int x;
 
 	while(*(src + 1) != '\0')
 	{
 		l++;
 	}
-	for ( ; x < 1; x++)
+	for (x = 0; x < l; x++)
 	{
 		dest[x] = src[x];
 	}
