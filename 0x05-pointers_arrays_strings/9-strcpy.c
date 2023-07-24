27ff0636d3fca0a2a69f1cef2d9bea0a0659116d
Gamalnabil <gamalnabil556@gmail.com>
2023-07-24 10:20:05 -0700

    Done

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copy the pointed to by src to dest
+ * @dest: the new pointed
+ * @src: the original pointed
+ * Return: Always dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int l = 0;
+	int x = 0;
+
+	while(*(src + 1) != '\0')
+	{
+		l++;
+	}
+	for ( ; x < 1; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[1] = '\0';
+	return (dest);
+}
