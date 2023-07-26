6788b4843b1a5114e59d105788d7f1a6ce582378
Gamalnabil <gamalnabil556@gmail.com>
2023-07-26 09:47:31 -0700

    Done

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,7 +16,7 @@ char *cap_string(char *str)
 		{
 			index++;
 		}
-		if (str[index - 1] == ' '||
+		if (str[index - 1] == ' ' ||
 		    str[index - 1] == '\t' ||
 		    str[index - 1] == '\n' ||
 		    str[index - 1] == ',' ||
@@ -33,7 +33,7 @@ char *cap_string(char *str)
 		{
 			str[index] -= 32;
 		}
-		index++
+		index++;
 	}
 	return (str);
 }
