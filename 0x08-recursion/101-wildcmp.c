838f8962f0d174970c7cf28c530ecec40bc9e588
Gamal Nabil <gamalnabil556@gmail.com>
2023-08-02 09:57:54 -0700

    Done

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -24,7 +24,7 @@ int inception(char *s1, char *s2)
 
 	if (*s1 == 0)
 		return (0);
-	if (*s1 == *s1)
+	if (*s1 == *s2)
 		ret += wildcmp(s1 + 1, s2 + 1);
 	ret += inception(s1 + 1, s2);
 	return (ret);
