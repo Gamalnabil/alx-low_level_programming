484108863b0c8ae70c5705a7c718bce261565ef4
Gamal Nabil <gamalnabil556@gmail.com>
2023-08-02 08:19:47 -0700

    Done

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * factorial - this function checks if the number > || < 0
+ * @n: input value
+ * Return: 0
+*/
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (1);
+	}
+}
