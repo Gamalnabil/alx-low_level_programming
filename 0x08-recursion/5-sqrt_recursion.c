f3fc317fc9ce5e638276098e5527b7837b35f8e0
Gamal Nabil <gamalnabil556@gmail.com>
2023-08-02 09:13:38 -0700

    Done

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,13 +12,13 @@ int _sqrt_recursion(int n)
 }
 int square(int n, int val)
 {
-	if (val * val = n)
+	if (val * val == n)
 	{
 		return (val);
 	}
 	else if (val * val < n)
 	{
-		return (square(n, val + 1);
+		return (square(n, val + 1));
 	}
 	else
 	{
